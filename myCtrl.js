c3cdd1bdd837fd52076cfeb45302281cb171b8a7
YuanGao0702 <gaoyuan0702@gwu.edu>
2019-07-25 09:41:09 -0400

    20190725

--- myCtrl.js
+++ myCtrl.js
@@ -1,8 +1,11 @@
 
-myapp.controller('myCtrl',function($scope){ 
-	$scope.name = 'Gary'
+myapp.controller('myCtrl',function($scope){
+	$scope.name = 'Gary';
 	$scope.names=[
 	{name:'Jani',country:'Norway'},
 	{name:'Hege',country:'Sweden'},
-	{name:'Kai',country:'Denmark'}]
+	{name:'Kai',country:'Denmark'}];
+	$scope.changeName = function(){
+		$scope.name = "Bei";
+	}
 })
